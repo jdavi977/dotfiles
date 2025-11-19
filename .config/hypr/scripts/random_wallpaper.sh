ed93747e63726e86d8b0945e44824f916624163e
John Lowen David <jdavi977@mtroyal.ca>
2025-11-18 20:02:58 -0700

    Removed .config folder from local repo

--- .config/hypr/scripts/random_wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WALLDIR="$HOME/.config/hypr/wallpapers"
-MONITOR=$(hyprctl monitors -j | jq -r '.[0].name')
-RANDOM_WALL=$(ls "$WALLDIR" | shuf -n 1)
-
-hyprctl hyprpaper wallpaper wallust "$MONITOR, $WALLDIR/$RANDOM_WALL"
